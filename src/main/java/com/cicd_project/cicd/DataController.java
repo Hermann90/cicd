8d9206ea458f235971754cbb9a39a48e429003ad
hermann90 <hermannchefouet@gmail.com>
2022-04-24 13:53:45 +0400

    update for sonar jenkins

--- src/main/java/com/cicd_project/cicd/DataController.java
+++ src/main/java/com/cicd_project/cicd/DataController.java
@@ -2,7 +2,10 @@ package com.cicd_project.cicd;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.github.javafaker.Currency;
 import com.github.javafaker.Faker;
+import com.github.javafaker.Nation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,11 +25,11 @@ public class DataController {
 
     @GetMapping("/nations")
     public JsonNode getRandomNations() {
-        var objectMapper = new ObjectMapper();
-        var faker = new Faker(new Locale("en-US"));
-        var nations = objectMapper.createArrayNode();
-        for (var i = 0; i < 10; i++) {
-            var nation = faker.nation();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Faker faker = new Faker(new Locale("en-US"));
+        ArrayNode nations = objectMapper.createArrayNode();
+        for (int i = 0; i < 10; i++) {
+            Nation nation = faker.nation();
             nations.add(objectMapper.createObjectNode()
                     .put("nationality", nation.nationality())
                     .put("capitalCity", nation.capitalCity())
@@ -38,11 +41,11 @@ public class DataController {
 
     @GetMapping("/currencies")
     public JsonNode getRandomCurrencies() {
-        var objectMapper = new ObjectMapper();
-        var faker = new Faker(new Locale("en-US"));
-        var currencies = objectMapper.createArrayNode();
-        for (var i = 0; i < 20; i++) {
-            var currency = faker.currency();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Faker faker = new Faker(new Locale("en-US"));
+        ArrayNode currencies = objectMapper.createArrayNode();
+        for (int i = 0; i < 20; i++) {
+            Currency currency = faker.currency();
             currencies.add(objectMapper.createObjectNode()
                     .put("name", currency.name())
                     .put("code", currency.code()));
